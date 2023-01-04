51021b7e0f86f5e68abe0a685573c330b5ea43ba
Xudongyu <2064355274@qq.com>
2023-01-04 10:46:33 +0800

    添加原始代码文件

---
+++ src/auxiliarytool.py
@@ -0,0 +1,18 @@
+import os
+# 利用mode的选择来确定你想实现的功能
+# 尽可能的将功能写的具有足够好的适用性
+# 找时间可以学习-help的写法
+# 应具备同步到GitHub的功能
+# 应当为每个mode提供足够多的使用说明
+# 应当可以利用cmake实现代码的编译
+# 类的定义和功能实现尽可能的放在一起
+
+# mode-1
+class GVMode1():
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
